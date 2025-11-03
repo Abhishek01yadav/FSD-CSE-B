9612919847507652126792b59722140938133252
Abhishek yadav <ay0180310@gmail.com>
2025-11-03 15:29:52 +0530

    shopping cart app

---
+++ Shopping-Vite-App/vite-project/src/assets/data/product.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+    id: 1,
+    name: "Shopping Cart",
+    price: 499,
+    image: "/react.svg" // Tumhare assets folder me react.svg hai
+  },
+  {
+    id: 2,
+    name: "Grocery Basket",
+    price: 299,
+    image: "/react.svg"
+  },
+  {
+    id: 3,
+    name: "Utility Cart",
+    price: 699,
+    image: "/react.svg"
+  }
+];
+
+export default products;
