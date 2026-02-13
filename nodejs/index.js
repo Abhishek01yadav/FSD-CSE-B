7c3379b42eab8fbcc471b626874ed99837b062f6
Abhishek yadav <ay0180310@gmail.com>
2026-02-13 12:12:54 +0530

    hi

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,4 +1,5 @@
 const http=require('http')
+const fs=require('fs');
 const myserver=http.createServer((req,res)=>{
 //    console.log("server1")
 //    res.end("this is my first server");
@@ -9,14 +10,59 @@ if(req.url=='/home'){
  else if(req.url=='/about us'){
     res.end("this is my about page")
 }
- else if(req.url=='/about'){
-    res.end(
-  <img src="https://images.unsplash.com/photo-1531131141161-ecdfb1858dd2?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGZsb3dlcnN8ZW58MHx8MHx8fDA=" />
-);
-    res.end("404 page is not found")
+else if(req.url=="/img"){
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+  res.end(`
+    <h2>Here is your image:</h2>
+    <img 
+      src="https://imgs.search.brave.com/R5KzBfBya-b4MZuFG75K-Tduj6r6vKpbEBi-TFSfO0U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFnZXMudW5zcGxhc2guY29tL3Bob3RvLTE1MzExMzExNDExNjEtZWNkZmIxODU4ZGQyP2ZtPWpwZyZxPTYwJnc9MzAwMCZhdXRvPWZvcm1hdCZmaXQ9Y3JvcCZpeGxpYj1yYi00LjEuMCZpeGlkPU0zd3hNakEzZkRCOE1IeHpaV0Z5WTJoOE1UUjhmR1pzYjNkLGNuTjhaVzU4TUh4OE1IeDhmREE9"
+      width="400"
+    />
+  `);
+    
+
 }
+ 
+else{
+    res.end("404 page is not found")}
+
 
 
 });
 
+
+ const result=fs.readFileSync("./cse-c.txt","utf-8");
+ console.log(result);
+
+
+
+//  fs.writeFile("./abc.txt", "hi we  are from abes", (err) => { 
+//   if(err) {
+//     console.log("error",err);
+    
+//   }
+// });
+
+// fs.readFile("./a.txt","utf-8",(err,result)=>{
+//   if(err) { 
+//     console.log("error",err);
+//   }
+
+//   else {
+//     console.log(result);
+//   }
+// });
+
+// fs.appendFileSync("./a.txt",` abes college is good`);
+// fs.appendFile("./a.txt",` abes college is good`, ()=>{});
+// fs.appendFile("./a.txt",` abes college is good`, (err)=>{
+//   if(err){
+//     console.log("error",err);
+    
+//   }
+// });
+
 myserver.listen(8000,()=>console.log("server is run"));
+
+
