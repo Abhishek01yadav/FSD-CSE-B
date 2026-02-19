83d4e82ea2671c0ab8b0e749e889b6a81598e9d5
Abhishek yadav <ay0180310@gmail.com>
2026-02-19 15:35:07 +0530

    55h

--- os/index.js
+++ os/index.js
@@ -1,11 +1,26 @@
-const os =require('os');
-console.log("plateform ",os.platform());
-console.log("userinfo",os.userInfo());
-console.log("CPU",os.arch());
-console.log("free memeory",os.freemem());
-console.log("total memeory",os.totalmem());
-console.log("uptime",os.uptime());
-console.log("home dir",os.homedir());
-console.log("host name ",os.hostname());
+// const os =require('os');
+// console.log("plateform ",os.platform());
+// console.log("userinfo",os.userInfo());
+// console.log("CPU",os.arch());
+// console.log("free memeory",os.freemem());
+// console.log("total memeory",os.totalmem());
+// console.log("uptime",os.uptime());
+// console.log("home dir",os.homedir());
+// console.log("host name ",os.hostname());
 
 
+
+const http=require('http');
+const fs=require('fs');
+const home =fs.readFileSync('./a.html');
+ const myserver=http.createServer((req,res)=>{
+    res.end(home);
+   
+   
+
+})
+
+myserver.listen(8000,()=>{
+    console.log("server is run");
+    
+})
