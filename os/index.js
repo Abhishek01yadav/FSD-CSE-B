27ef5cc08736938c5aa8f1f907ae39cee201199c
Abhishek yadav <ay0180310@gmail.com>
2026-02-19 14:37:24 +0530

    njhjh

--- os/index.js
+++ os/index.js
@@ -2,4 +2,9 @@ const os =require('os');
 console.log("plateform ",os.platform());
 console.log("userinfo",os.userInfo());
 console.log("CPU",os.arch());
+console.log("free memeory",os.freemem());
+console.log("total memeory",os.totalmem());
+console.log("uptime",os.uptime());
+console.log("home dir",os.homedir());
+
 
